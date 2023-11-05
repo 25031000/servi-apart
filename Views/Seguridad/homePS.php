26ec53f1b41961413228c62dda18e7c3933dac87
Camilo Pinilla <camilo.pinilla2503@gmail.com>
2023-11-04 20:12:26 -0500

    refactor: :art: Cambio de UI en home seguirdad

--- Views/Seguridad/homePS.php
+++ Views/Seguridad/homePS.php
@@ -18,8 +18,7 @@ require_once("../../Controllers/mostrarInfoAdmin.php");
     <!-- <link href="../../assets/css/style.css" rel="stylesheet"> -->
 
     <!-- bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
     <!-- Custom styles -->
     <link rel="stylesheet" href="../../components/css/header.css">
@@ -40,78 +39,52 @@ require_once("../../Controllers/mostrarInfoAdmin.php");
     include '../../components/menuPS.php';
     include '../../components/headerIncludePS.php';
     ?>
-    <div class="custom-mouse d-flex justify-content-center align-items-center">
-        <span class="text-black fw-bolder clicki">CLICK</span>
-    </div>
 
-
-    <main id="dash-container" class="container-fluid position-relative">
-        <section class="mt-5">
-            <div class="portada text-center d-flex align-items-center justify-content-center">
-                <h3 class="seguridad-titulo text-center w-100">SEGURIDAD</h3>
+    <main id="dash" class="container position-relative">
+        <div class="w-100 mt-5">
+            <h2 style="font-size: 2.5rem; letter-spacing: 5px" class="text-center ">Personal de seguridad</h2>
+            <p class="text-center" style="font-family: 'Roboto', sans-serif !important; font-weight: 300 !important;">Navega entre los distintos modulos en base a las actividades que necesisas realizar</p>
+        </div>
+        <section class="grid  mx-auto mt-5" style="height: 670px;">
+            <div class="rounded-4 position-relative d-flex flex-column justify-content-center">
+                <h2 class="ps-4">Vehiculos</h2>
+                <p class="ps-4">Manten un registro minusioso de los vehiculos que se encuentran en la propiedad y controla su estado de una manera rapida</p>
+                <span role="button" class="p-1 position-relative rounded-5 justify-content-center align-items-center ms-4 d-flex " style="width: 50px; height:50px; top: 30px; background: #FFA031">
+                    <img src="./images/Next.png" alt="">
+                </span>
+                <img src="./images/carro.png" class="position-absolute" alt="">
             </div>
-        </section>
-        <section class="mt-5 pt-5">
-            <div class="row px-3   justify-content-center gap-5">
-                <section role="button" class="col-md-5 module  p-0  d-flex flex-column justify-content-center ">
-                    <div class="">
-                        <img class="w-100  rounded-top img-module h-100" src="./images/estacionamiento.jpg" alt="">
-                    </div>
-                    <div class="border text-center fs-3 d-flex rounded-bottom py-3">
-                        <a href="ver-vehiculo.php" class="p-0 w-100 mb-0 publico"
-                            style="text-decoration: none; color: black;">Vehículos</a>
-                    </div>
-                </section>
-                <section role="button" class="col-md-5 module  p-0 d-flex flex-column ">
-                    <div class="m-0">
-                        <img class="w-100  rounded-top img-module h-100" src="./images/paqueteria.jpg" alt="">
-                    </div>
-                    <div class="border  text-center align-items-center  fs-3 d-flex rounded-bottom m-0  py-3">
-                        <a href="paqueteria.php" class="p-0 w-100 mb-0 publico"
-                            style="text-decoration: none; color: black;">paqueteria</a>
-                    </div>
-                </section>
-
+            <div class="rounded-4 position-relative d-flex flex-column justify-content-center">
+                <h2 class="ps-4">Salon comunal</h2>
+                <p class="ps-4">Mantente al tanto de que personas estan utilizando el salon comunal y cuales son sus horarios.</p>
+                <span role="button" class="p-1 position-relative rounded-5 justify-content-center align-items-center ms-4 d-flex " style="width: 50px; height:50px; top: 30px; background: #05EB00">
+                    <img src="./images/Next.png" alt="">
+                </span>
+                <img src="./images/calendar.png" style="width: 158px;" class="position-absolute" alt="">
             </div>
-            <div class="row px-3 mt-5 justify-content-center gap-5">
-                <section role="button" class="col-md-5 module  p-0 d-flex flex-column justify-content-center ">
-                    <div class="">
-                        <img class="w-100  rounded-top img-module h-100" src="./images/saloncomunal.jpg" alt="">
-                    </div>
-                    <div class="border text-center  align-items-center fs-3 d-flex  rounded-bottom py-3">
-                        <a href="salonComunal.php" class="p-0 w-100 mb-0 publico"
-                            style="text-decoration: none; color: black;">Salon Comunal</a>
-
-                    </div>
-                </section>
-
-
-                <section role="button" class="col-md-5 module   p-0 d-flex flex-column ">
-                    <div class="">
-                        <a href="ver-publicaciones.php">
-                            <img class="w-100 rounded-top img-module h-100" src="./images/publicaciones.jpg" alt="">
-                        </a>
-                    </div>
-                    <div class="border text-center fs-3 d-flex rounded-bottom py-3">
-                        <a href="ver-publicaciones.php" class="p-0 w-100 mb-0 publico"
-                            style="text-decoration: none; color: black;">Publicaciones</a>
-                    </div>
-                </section>
-
+            <div class="rounded-4 position-relative d-flex flex-column justify-content-center">
+                <h2 class="ps-4">Paqueteria</h2>
+                <p class="ps-4">Registra cada paquete que llegue a porteria y hazle saber a los residentes su llegada.</p>
+                <span role="button" class="p-1 position-relative rounded-5 justify-content-center align-items-center ms-4 d-flex " style="width: 50px; height:50px; top: 30px; background: #7D68FF">
+                    <img src="./images/Next.png" alt="">
+                </span>
+                <img src="./images/paquete.png" style="width: 158px;" class="position-absolute" alt="">
+            </div>
+            <div class="rounded-4 position-relative d-flex flex-column justify-content-center">
+                <h2 class="ps-4">Publicaciones</h2>
+                <p class="ps-4">Visualiza las publicaciones que la administracion realiza para mantenerte al dia.</p>
+                <span role="button" class="p-1 position-relative rounded-5 justify-content-center align-items-center ms-4 d-flex " style="width: 50px; height:50px; top: 30px; background: #2FA3FF">
+                    <img src="./images/Next.png" alt="">
+                </span>
+                <img src="./images/bocina.png" style="width: 158px;" class="position-absolute" alt="">
             </div>
         </section>
     </main>
-    <footer class=" border container-fluid py-3 mt-5 bg-light">
-        <div class="w-100">
-            <p>2023 © Admin Board. - <a href="#">Servi-Apart.</a></p>
-        </div>
-    </footer>
 
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
     <!-- Common -->
     <script src="../Dashboard/js/lib/jquery.min.js"></script>
@@ -121,33 +94,9 @@ require_once("../../Controllers/mostrarInfoAdmin.php");
     <script src="../Dashboard/js/lib/bootstrap.min.js"></script>
 
 
-    <script>
-        document.addEventListener("mousemove", (e) => {
-            let mouseX = e.clientX;
-            let mouseY = e.clientY;
 
-            $('.custom-mouse').css({
-                transform: `translate(${mouseX}px, ${mouseY}px)`,
-            })
-        })
-
-        $('.img-module').mouseover((e) => {
-            $('.custom-mouse').css({
-                opacity: 1
-            })
-        })
-
-        $('.img-module').mouseleave((e) => {
-            $('.custom-mouse').css({
-                opacity: 0
-            })
-        })
-
-
-
-    </script>
     <script src="../Dashboard/js/scripts.js"></script>
-   
+
 </body>
 
 </html>
